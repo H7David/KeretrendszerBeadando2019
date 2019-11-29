ef9cc67c5527934b34e55496c8126761343bff5d
H7David <43805249+H7David@users.noreply.github.com>
2019-11-29 23:04:48 +0100

    DAO-json and gitignor letrehoas/ modosita

--- core/src/main/java/motorbikes/model/Motor.java
+++ core/src/main/java/motorbikes/model/Motor.java
@@ -152,5 +152,22 @@ public class Motor {
     public void setAllapot(Allapot allapot) {
         this.allapot = allapot;
     }
+    @Override
+    public String toString() {
+        return "Auto{" +
+                "marka='" + marka + '\'' +
+                ", tipus='" + tipus + '\'' +
+                ", rendszam='" + rendszam + '\'' +
+                ", uzembehelyezes_datuma=" + uzembehelyezes_datuma +
+                ", loero=" + loero +
+                ", suly=" + suly +
+                ", sebessegek_szama=" + sebessegek_szama +
+                ", szallithatoszemelyek_szsma=" + szalithatoszemelyek_szama +
+                ", kerek_atmero=" + kerek_atmero +
+                ", evjarat=" + evjarat +
+                ", szinkod='" + szinkod + '\'' +
+                ", allapot=" + allapot +
+                '}';
+    }
 
 }
