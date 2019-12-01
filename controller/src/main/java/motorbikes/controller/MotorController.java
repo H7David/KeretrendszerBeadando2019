0f1f4aa6f628db6f708c66bbefd2301dd6dc4d26
H7David <43805249+H7David@users.noreply.github.com>
2019-12-01 14:23:17 +0100

    controller and service supplemented

--- controller/src/main/java/motorbikes/controller/MotorController.java
+++ controller/src/main/java/motorbikes/controller/MotorController.java
@@ -1,10 +1,21 @@
 package motorbikes.controller;
 
+import motorbikes.service.MotorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MotorController {
+
+    MotorService service;
+    public  MotorController(@Autowired(required = true) MotorService service) { this.service = service; }
+
+
     @RequestMapping(value = "kutya")
     public void proba(){System.out.println("Működik!!!");}
+
+
+    @RequestMapping(value = "test")
+    public void testService(){ System.out.println(service.listAllMotors().size()); }
 }
